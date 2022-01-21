8b3c359692df969d4f44fb1912cb40ec5a4fd626
alexis-netbook <alex3000@live.com.ar>
2022-01-20 21:49:05 -0300

    falta arreglar algo del first()

--- blog/app/Http/Controllers/UsersController.php
+++ blog/app/Http/Controllers/UsersController.php
@@ -3,16 +3,59 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+//use Illuminate\Http\Client\Request;
+use Illuminate\Support\Str;
+
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
-   function index(){
+   function index(Request $request){
        //return "Users";
       // $user = new User();
       // $user-> name = 'Belana';
       // $user-> email = 'belatorres@hotmail.com';
       // $user->password
-      $users = User::all();
-       return response()->json([$users], status:200);
+      if ($request->isJson()) {
+        $users = User::all();
+        return response()->json($users, status:200);
+      }
+      return response()->json(['error' => 'Unaathorized'], status:401);
    }
+   function createUser(Request $request){
+    if ($request->isJson()) {
+     //TODO: Create the user in the DB
+      
+        $data = $request->json()->all();
+        $user = User::create([
+           'name' => $data['name'],
+           'username' => $data['username'],
+           'email' => $data['email'],
+           'password' => Hash::make($data['password']),
+           'api_token' => Str::random(60),
+        ]);
+     return response()->json($user, status:201);
+   }
+   return response()->json(['error' => 'Unaathorized'], status:401);
+}
+
+function getToken(Request $request) {
+   if($request->isJson()){
+      try{
+         $data = $request->json()->all();
+         $user = User::where(column: 'username',  $data['username'])->first();
+         if($user && Hash::make($data['password'], $user->password)){
+            return response()->json($user, status:200);
+         }
+         else{
+            return response()->json(['error'=> 'No content'], status:406);
+            }
+      }catch (ModelNotFoundException $e){
+         return response()->json(['error'=> 'No content'], status:406);
+      }
+   }
+   return response()->json(['error' => 'Unaathorized'], status:401);
+}
 }
